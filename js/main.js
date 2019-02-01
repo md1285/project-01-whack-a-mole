7e528b3bae2e98f3cdbc3e300c8784fb90844762
Michael Dwyer <michaeldwyer@Michaels-MacBook-Pro-3.local>
2019-02-01 14:59:48 -0800

    adjust layout and styling and implement speedup feature

--- js/main.js
+++ js/main.js
@@ -1,105 +1,111 @@
-
-    
-
-
-/*----- constants -----*/ 
+/*----- constants -----*/
 const moleCellsArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+const MAX_TIME = 30;
+const ONE_SECOND = 1000;
 
-/*----- app's state (variables) -----*/ 
-let countdown;
-let score;
-let missed;
-let gameStart;
+/*----- app's state (variables) -----*/
+let countdown, score, missed, timerStarted, molesStarted, disappearSpeed, popupSpeed;
 
 
-/*----- cached element references -----*/ 
+/*----- cached element references -----*/
 const $startButton = $('#start-button')
 const $board = $('table');
+const $timerDisplay = $('#timer-display');
+const $scoreDisplay = $('#score-display');
+const $missedDisplay = $('#missed-display');
 
 
 
-/*----- event listeners -----*/ 
+/*----- event listeners -----*/
 $startButton.on('click', startGame);
 $board.on('click', whackMole)
 
 
 /*----- functions -----*/
-function init () {
-    countdown = 30;
+function init() {
+    countdown = MAX_TIME;
     score = 0;
     missed = 0;
+    disappearSpeed = 1000;
+    popupSpeed = 1500;
     timerStarted = false;
     molesStarted = false;
     render();
 }
 
 function startGame() {
-    startTimer();
-    startMoles();
-}
-
-function startTimer(){
-
-    if (timerStarted === false) {
-        timerStarted = true;
-        setInterval(function(){
-            if (countdown >= 0){
-                let timerDisp = countdown.toString()
-                $('#timer-display').text(timerDisp);
-                countdown--;
-            }
-        }, 1000);  
+    if (!timerStarted) {
+        startTimer();
     }
-
-
+    if (!molesStarted) {
+        startMoles();
+    }
 }
 
-function startMoles(){
+function startTimer() {
+    timerStarted = true;
+    setInterval(function () {
+        if (countdown >= 0) {
+            $timerDisplay.text(countdown.toString());
+            countdown--;
+        }
 
-    if (molesStarted === false){
-        molesStarted = true;
-        setInterval(function(){
-            if (countdown >= 0){
-            let randNum = parseInt(Math.random() * 11.9999999);
+        if (countdown < 20 && countdown >= 10) {
+            disappearSpeed = 750;
+            popupSpeed = 1000;
+        } else if (countdown < 10 && countdown >= 0){
+            disappearSpeed = 500;
+            popupSpeed = 501;
+        }
+    }, 1000);
+}
 
-            if (moleCellsArray[randNum] === 0){
-                moleCellsArray[randNum] = 1;
-                setTimeout(function (){
-                    if (moleCellsArray[randNum] === 1){
-                        moleCellsArray[randNum] = 0;
-                        missed++;
-                        render();
-                    }
-                }, 1000);
-            }
-
+function startMoles() {
+    molesStarted = true;
+    setInterval(function () {
+        if (countdown >= 0) {
+            let randNum = Math.floor((Math.random() * 12));
+            moleCellsArray[randNum] = 1;
+            setTimeout(function () {
+                if (moleCellsArray[randNum] === 1) {
+                    moleCellsArray[randNum] = 0;
+                    missed++;
+                    render();
+                }
+            }, disappearSpeed);
             render();
-            }
-
-        }, 1500);
-    } 
-
+        }
+    }, popupSpeed);  
 }
 
 
 function whackMole(evt) {
-    if (moleCellsArray[evt.target.value] === 1){
+    if (moleCellsArray[evt.target.value] === 1) {
         moleCellsArray[evt.target.value] = 0;
         score++;
-        render();
     }
+    render();
 }
 
-function render(){
-    moleCellsArray.forEach(function(cell, idx){
-        if (cell === 1){
-            $(`button[value="${idx}"]`).css({backgroundColor: 'brown'});
+function render() {
+    moleCellsArray.forEach(function (cell, idx) {
+        if (cell === 1) {
+            $(`button[value="${idx}"]`).css({
+                background: 'url(images/mole2.png)',
+                'background-position': 'center',
+                'background-size': '90%',
+                'background-repeat': 'no-repeat',
+                'outline': 'none'
+            });
         } else {
-            $(`button[value="${idx}"]`).css({backgroundColor: '#DDDDDD'});
+            $(`button[value="${idx}"]`).css({
+                background: 'black',
+                'outline': 'none'
+            });
         }
     });
-    $('#score-display').text(score);
-    $('#missed-display').text(missed);
+    $scoreDisplay.text(score);
+    $missedDisplay.text(missed);
 }
 
 
