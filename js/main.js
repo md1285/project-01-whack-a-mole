e386f4be42e967bbe82e3a2283bc834ca352898a
Michael Dwyer <michaeldwyer@Michaels-MacBook-Pro-3.local>
2019-02-05 14:18:49 -0800

    finish styling and add high score

--- js/main.js
+++ js/main.js
@@ -1,23 +1,34 @@
 /*----- constants -----*/
 const moleCellsArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 const MAX_TIME = 30;
+const sounds = {
+    molePopupSound: "./sfx/01-popup.mp3",
+    malletWhackSound: "./sfx/02-mallet-whack.mp3",
+    missWhackSound: './sfx/03-dull-whack.mp3',
+    startBellSound: './sfx/05-start-bell.mp3'
+};
 
 /*----- app's state (variables) -----*/
-let countdown, score, missed, timerStarted, molesStarted, disappearSpeed, popupSpeed;
-
+let countdown, score, missed, timerStarted, molesStarted, disappearSpeed, popupSpeed, bellOn, highScore;
+//highScore is not in init function because it does NOT get reset. will need to cache it to local storage later.
+highScore = 0;
 
 /*----- cached element references -----*/
-const $startButton = $('#start-button')
-const $board = $('table');
+const $startButton = $('footer section button')
+const $board = $('#game-container');
 const $timerDisplay = $('#timer-display');
 const $scoreDisplay = $('#score-display');
 const $missedDisplay = $('#missed-display');
-
-
+const $body = $('body');
+const player = new Audio();
+const molePopupPlayer = new Audio();
+const $highScoreDisplay = $('#high-score-display');
 
 /*----- event listeners -----*/
+$startButton.on('touchstart', renderStartButtonTap);
 $startButton.on('click', startGame);
 $board.on('click', whackMole)
+$body.on('click', clickOffBoard);
 
 
 /*----- functions -----*/
@@ -25,6 +36,7 @@ function init() {
     countdown = MAX_TIME;
     score = 0;
     missed = 0;
+    bellOn = true;
     timerStarted = false;
     molesStarted = false;
     disappearSpeed = 1000;
@@ -33,9 +45,17 @@ function init() {
 }
 
 function startGame() {
-    if (!timerStarted) startTimer();
+    $startButton.removeAttr('id');
+    if (bellOn === true){
+        bellOn = false;
+    }
+    if (!timerStarted) {
+        startTimer();
+    } else {
+        playMissWhackSound();
+    }
     if (!molesStarted) startMoles();
-    if (countdown < 0){
+    if (countdown < 0) {
         init();
         startTimer();
         startMoles();
@@ -43,11 +63,11 @@ function startGame() {
 }
 
 function startTimer() {
+    playStartBellSound();
     timerStarted = true;
     let clockTimer = setInterval(function () {
         if (countdown >= -1) {
-            //this is the one place I'm modifying the dom outside of render. workaround?
-            $timerDisplay.text(countdown.toString());
+            renderClock();
             countdown--;
         }
         if (countdown < 20 && countdown >= 10) {
@@ -59,9 +79,11 @@ function startTimer() {
         }
         if (countdown < 0) {
             clearInterval(clockTimer);
+            bellOn = true;
+            $startButton.attr('id', 'start-button-click');
+            render();
         }
     }, 1000);
-
 }
 
 function startMoles() {
@@ -76,6 +98,7 @@ function startMoles() {
                 render();
             }
         }, disappearSpeed);
+        playMolePopupSound();
         render();
         if (countdown <= 0) {
             clearInterval(moleCounter);
@@ -83,15 +106,19 @@ function startMoles() {
     }, popupSpeed);
 }
 
-
 function whackMole(evt) {
+    if (moleCellsArray[evt.target.value] === 0) {
+        playMissWhackSound();
+    } 
     if (moleCellsArray[evt.target.value] === 1) {
         moleCellsArray[evt.target.value] = 0;
         score++;
+        updateHighScore();
+        playMalletWhackSound();
         $(`button[value="${evt.target.value}"]`).css({
             border: `6px solid white`
         });
-        setTimeout(function(){
+        setTimeout(function () {
             $(`button[value="${evt.target.value}"]`).css({
                 border: `2px solid white`
             });
@@ -99,6 +126,15 @@ function whackMole(evt) {
     }
     render();
 }
+
+function updateHighScore(){
+    if (score > highScore){
+        highScore++;
+        render();
+    }
+}
+
+/*----- Render functions -----*/
 
 function render() {
     moleCellsArray.forEach(function (cell, idx) {
@@ -108,22 +144,81 @@ function render() {
                 'background-position': 'center',
                 'background-size': '90%',
                 'background-repeat': 'no-repeat',
-                'outline': 'none'
             });
         } else {
             $(`button[value="${idx}"]`).css({
                 background: 'black',
-                'outline': 'none',
             });
         }
     });
-
     $scoreDisplay.text(score);
     $missedDisplay.text(missed);
+
+    if (countdown < 0) {
+        setTimeout(function(){
+            $startButton.text('TRY AGAIN');
+        }, 1000);
+    }
+
+    $highScoreDisplay.text(highScore);
+
+}
+
+function renderClock() {
+    $timerDisplay.text(countdown.toString());
+}
+
+function renderStartButtonTap() {
+    if ($startButton[0].hasAttribute('id')){
+        $startButton.css({
+            color: 'black',
+            'background-color': 'white'
+        });
+        setInterval(function(){
+            $startButton.css({
+                color: 'white',
+                'background-color': 'black'
+            });
+        }, 150);
+    }   
+}
+
+
+/*----- Sound functions -----*/
+
+function playMolePopupSound() {
+    molePopupPlayer.src = sounds.molePopupSound;
+    molePopupPlayer.volume = 0.9;
+    molePopupPlayer.play();
+}
+
+function playMalletWhackSound() {
+    player.src = sounds.malletWhackSound;
+    player.volume = 0.5;
+    player.play();
+}
+
+function playMissWhackSound() {
+    player.src = sounds.missWhackSound;
+    player.volume = 1;
+    player.play();
+}
+
+function playStartBellSound(){
+    player.src = sounds.startBellSound;
+    player.volume = 1;
+    player.play();
+}
+
+function clickOffBoard(evt) {
+    if (evt.target.className !== 'click-button start-button' && evt.target.className !== 'click-button'){
+        playMissWhackSound();
+    } 
 }
 
 
 
-/*----- Initialize Game -----*/
 
+
+/*----- Initialize Game -----*/
 init();
